a31824c945795b6e6b627a8ed5a22be9ddbe3f9d
Swall0w <technext.jpn@gmail.com>
2017-04-15 22:16:02 +0900

    add status in loop

--- pash/shell.py
+++ pash/shell.py
@@ -1,6 +1,15 @@
 # -*- coding:utf-8 -*-
+import sys
+
+shell_status_run = True
+shell_status_stop = False
 def loop():
-    pass
+    status = shell_status_run
+    while status == shell_status_run:
+        sys.stdout.write('> ')
+        sys.stdout.flush()
+
+        cmd = sys.stdin.readline()
 def main():
     loop() 
 
