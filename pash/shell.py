fe6c059b200b9b0a324cfa2cd9df1700fe23c6f3
Swall0w <technext.jpn@gmail.com>
2017-04-15 22:26:34 +0900

    tokenize

--- pash/shell.py
+++ pash/shell.py
@@ -6,10 +6,18 @@ shell_status_stop = False
 def loop():
     status = shell_status_run
     while status == shell_status_run:
+# Display a command prompt
         sys.stdout.write('> ')
         sys.stdout.flush()
-
+# Read command input
         cmd = sys.stdin.readline()
+# Tokenize the command input
+        cmd_tokens = _tokenize(cmd)
+# Execute the command and retrieve new status
+        status = _execute(cmd_tokens)
+def _tokenize(cmd):
+    pass
+def _execute(cmd):
 def main():
     loop() 
 
