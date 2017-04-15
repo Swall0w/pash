2f980174333f9e980c2a7d3af64216b03ba0e396
Swall0w <technext.jpn@gmail.com>
2017-04-15 23:05:18 +0900

    added fork

--- pash/shell.py
+++ pash/shell.py
@@ -1,6 +1,7 @@
 # -*- coding:utf-8 -*-
 import sys
 import shlex
+import os
 
 shell_status_run = True
 shell_status_stop = False
@@ -20,7 +21,30 @@ def loop():
 def _tokenize(string):
     return shlex.split(string)
 def _execute(cmd):
-    pass
+    ''' 
+    Fork a child shell process
+    If the current process is a child process, its pid is set to 0
+    else the current process is a parent process and the value of pid
+    is the process id of its child process.
+    '''
+    pid = os.fork()
+    if pid == 0:
+    # Child process
+        # Replace the child shell process with the program called with exec
+        os.execvp(cmd[0],cmd)
+    elif pid > 0:
+    # Parent process
+        while True:
+            # Wait response status from its child process (identifed with pid)
+            wpid, status = os.waitpid(pid, 0)
+            # Finish waiting if its child process exits normally
+            # or is terminated by a signal
+            if os.WIFEXITED(status) or os.WIFSIGNALED(status):
+                break
+
+    # Return status indicating to wait for next command in loop
+    return shell_status_run
+
 def main():
     loop() 
 
